2609d51d7c414962d4f2068b003edd2a0fa0fe35
HanzChristian <hanz.christian@ui.ac.id>
2022-07-22 12:34:42 +0700

    initial Commit

---
+++ NC2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NC2
+//
+//  Created by Hanz Christian on 22/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            Text("hello")
+            Text("Hello, world!")
+                .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NC2/NC2App.swift
@@ -0,0 +1,17 @@
+//
+//  NC2App.swift
+//  NC2
+//
+//  Created by Hanz Christian on 22/07/22.
+//
+
+import SwiftUI
+
+@main
+struct NC2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
