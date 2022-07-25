733c47a29bc3f54bf59e98151e5b4341e9188be5
HanzChristian <hanz.christian@ui.ac.id>
2022-07-25 17:38:27 +0700

    Added Card & Gesture, Logic for Repetitive Card

--- NC2/DeckView.swift
+++ NC2/DeckView.swift
@@ -10,29 +10,6 @@ import SwiftUI
 // 1st View
 struct DeckView: View {
     
-    //Dummy data
-    @State var all_contents = [
-        Content(id: 0, type: "Introduction", category: "Introducing Yourself", words: "For those of you who don’t know me already, my name’s ... and I’m responsible for...", place: 1),
-        
-        Content(id: 1, type: "T", category: "Asada", words: "Konz",place: 2),
-        
-        Content(id: 1, type: "e", category: "Sama", words: "Lu",place: 2),
-        
-        Content(id: 1, type: "s", category: "Semua", words: "Ngen",place: 2),
-        
-        Content(id: 1, type: "t", category: "Ngen", words: "Sama",place: 2),
-        
-        Content(id: 1, type: "i", category: "Lu", words: "Semua",place: 2),
-        
-        Content(id: 1, type: "s", category: "Inilah", words: "Pahitnya",place: 2),
-    ]
-    
-    @State var isActive: Bool = false
-    
-    func buttonPressed(){
-        self.isActive = true
-    }
-    
     private let columns = [
         GridItem(.adaptive(minimum: 150))
     ]
@@ -52,19 +29,13 @@ struct DeckView: View {
                 
                 ScrollView{
                     LazyVGrid(columns: columns, spacing: 20){
-                        ForEach(1..<7){ i in
+                        ForEach(0..<6){ i in
                             HStack{
-                                
-                                NavigationLink(destination: CardView(), isActive: self.$isActive){
-                                    Text("")
+                                NavigationLink(destination: CardView(index:i)){
+                                        Image("img\(i + 1)")
+                                            .resizable().frame(width: 150, height: 201)
                                 }
                                 
-                                Button(action:{
-                                    buttonPressed()
-                                }){
-                                    Image("img\(i)")
-                                        .resizable().frame(width: 150, height: 201)
-                                }
                             }
                         }
                     }
@@ -85,12 +56,52 @@ struct CardView:View{
     
     var arrayOfContent:[String] = ["Product","Decision Making","Motivational","Persuasuve","Progress","Instructive"]
     
+    //Determines how many card are swiped
+    @State var swiped = 0
+    @State var all_contents = [
+        Content(id: 0, type: "Introduction", category: "Introducing Yourself", words: "For those of you who don’t know me already, my name’s ... and I’m responsible for...",offset: 0,place: 1),
+
+        Content(id: 1, type: "Introduction", category: "Introducing Yourself", words: "For those of you who don’t know me already, my name’s ... and I’m responsible for...",offset: 0,place: 2),
+        
+        Content(id: 2, type: "Introduction", category: "Introducing Yourself", words: "For those of you who don’t know me already, my name’s ... and I’m responsible for...",offset: 0,place: 3),
+        
+        Content(id: 3, type: "Introduction", category: "Introducing Yourself", words: "For those of you who don’t know me already, my name’s ... and I’m responsible for...",offset: 0,place: 4),
+        
+        Content(id: 4, type: "Introduction", category: "Introducing Yourself", words: "For those of you who don’t know me already, my name’s ... and I’m responsible for...",offset: 0,place: 5),
+    
+        Content(id: 5, type: "Introduction", category: "Introducing Yourself", words: "For those of you who don’t know me already, my name’s ... and I’m responsible for...",offset: 0,place: 6),
+
+        Content(id: 6, type: "Introduction", category: "Introducing Yourself", words: "For those of you who don’t know me already, my name’s ... and I’m responsible for...",offset: 0,place: 7),
+    ]
+    
+    
+    
+    //to loop the cards
+    func restoreCard(id: Int){
+       var currentCard = all_contents[id]
+        
+        currentCard.id = all_contents.count
+        all_contents.append(currentCard)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3){
+                all_contents[all_contents.count - 1].offset = 0
+        }
+    }
+    
+    //swipe rotate animation
+    func getRotation(offset: CGFloat)->Double{
+        let value = offset/150
+        let angle:CGFloat = 10
+        
+        let degree = Double(value * angle)
+        
+        return degree
+    }
+    
     var index:Int = 0
     
     var body: some View{
-        
         //logic berdasarkan index
-        if(index == 0){
             VStack{
                 Rectangle()
                     .frame(height: 0)
@@ -98,15 +109,43 @@ struct CardView:View{
                 Spacer()
                 
                 GeometryReader{reader in
-                    
                     ZStack{
-//                        ForEach(all_cont)
-                        
-                    }
+                        if(index == 0){
+                            ForEach(all_contents.reversed()){Content in
+                                ContentView(Content: Content, reader: reader,swiped: $swiped)
+                                //for swipe gesture
+                                    .offset(x: Content.offset)
+                                    .rotationEffect(.init(degrees: getRotation(offset: Content.offset)))
+                                
+                                    .gesture(DragGesture().onChanged({ (value) in
+                                        
+                                        withAnimation{
+                                            if (value.translation.width > 0){
+                                                all_contents[Content.id].offset = value.translation.width
+                                            }
+                                        }
+                                    }).onEnded({ (value) in
+                                        
+                                        withAnimation{
+                                            if (value.translation.width > 150){
+                                                all_contents[Content.id].offset = 1000
+                                                swiped = Content.id + 1
+                                                restoreCard(id: Content.id)
+                                            }
+                                            else{
+                                                all_contents[Content.id].offset = 0
+                                            }
+                                        }
+                                    }))
+                            }
+                        }
+                    } //zstack
+                    .offset(y: -25)
                 }
             }
             
-            .navigationBarTitle(Text("\(arrayOfContent[0])")).navigationBarHidden(false)
+            
+            .navigationBarTitle(Text("\(arrayOfContent[index])")).navigationBarHidden(false)
             .background(Color(hex: "FEFBE7"))
             
             //Changes to back button color
@@ -122,8 +161,84 @@ struct CardView:View{
                     
                 }
             })
+    }
+}
+
+struct ContentView: View{
+    
+    var Content: Content
+    var reader: GeometryProxy
+    @Binding var swiped: Int
+    
+    var body: some View{
+        VStack{
             
+            VStack{
+                HStack{
+                    VStack(alignment: .leading){
+                        Text("\(Content.type)")
+                            .font(.system(size: 46))
+                            .fontWeight(.bold)
+                            .foregroundColor(Color(hex: "476F78"))
+                            .padding(.leading,20)
+                        
+                        Text("Category :")
+                            .font(.system(size: 16))
+                            .fontWeight(.bold)
+                            .foregroundColor(Color(hex: "476F78"))
+                            .padding(.leading,20)
+                        
+                        Text("\(Content.category)")
+                            .font(.system(size: 16))
+                            .fontWeight(.bold)
+                            .foregroundColor(Color(hex: "476F78"))
+                            .padding(.leading,20)
+                            .padding(.bottom,25)
+                        
+                        Text("\(Content.words)")
+                            .font(.system(size: 30))
+                            .fontWeight(.bold)
+                            .foregroundColor(Color(hex: "476F78"))
+                            .padding(.leading,20)
+                            .padding(.bottom,20)
+                        
+                        HStack{
+                            Text("#")
+                                .font(.system(size: 48))
+                                .fontWeight(.bold)
+                                .foregroundColor(Color(hex: "476F78"))
+                            
+                            Text("\(Content.place)")
+                                .font(.system(size: 48))
+                                .fontWeight(.bold)
+                                .foregroundColor(Color(hex: "476F78"))
+                        }
+                        .padding(.leading,250)
+                        .padding(.top,50)
+                        Spacer(minLength: 0)
+                    }
+                }
+                .padding(.bottom,20)
+                .padding(.top,25)
+            }
+            .frame(height: reader.frame(in: .global).height - 180)
+            .background(Color(hex: "F9EBC8"))
+            .cornerRadius(20)
+            .padding(.vertical,10)
+            .padding(.horizontal,25)
+            .padding(.top,50)
+            .shadow(color: Color.black.opacity(0.05), radius: 5, x: 0, y: 5)
+            //Get next content with 1:2 comparison & 2 cards displayed
+            .offset(y: Content.id - swiped <= 2 ? CGFloat(Content.id - swiped) * 25 : 50)
+            
+            Spacer(minLength: 0)
+                
         }
+        
+        
+        
+        //Content for drag/swipe gesture
+//        .contentShape(Rectangle())
     }
 }
 
@@ -134,7 +249,6 @@ struct ContentView_Previews: PreviewProvider {
         DeckView()
     }
 }
-
 
 
 //To add hex in color
@@ -171,6 +285,7 @@ struct Content: Identifiable{
     var type : String
     var category : String
     var words: String
+    var offset: CGFloat
     var place: Int
 }
 
--- NC2/Models/contentData.swift
+++ NC2/Models/contentData.swift
@@ -1,47 +1,47 @@
+////
+////  contentData.swift
+////  NC2
+////
+////  Created by Hanz Christian on 22/07/22.
+////
 //
-//  contentData.swift
-//  NC2
+//import Foundation
+//import CoreData
+//import SwiftUI
 //
-//  Created by Hanz Christian on 22/07/22.
+//class CoreDataManager{
+//    let context =
+//    DeckView().environment(\.managedObjectContext, persistenceController.container.viewContext)
+//    }
 //
-
-import Foundation
-import CoreData
-import SwiftUI
-
-class CoreDataManager{
-    let context =
-    DeckView().environment(\.managedObjectContext, persistenceController.container.viewContext)
-    }
-
-var items:[Deck]?
-
-func dummyData(){
-    
-    // create medicine
-    let deck = Deck(context: context)
-    deck1.deckName = "Product"
+//var items:[Deck]?
+//
+//func dummyData(){
+//
+//    // create medicine
 //    let deck = Deck(context: context)
-//    deck2.deckName = "Product"
-    
-    // Add time
-    let card1 = Card(context: context)
-    card1.cardCategory = "Introducing"
-    card1.cardIndex = 1
-    card1.cardTitle = "Product"
-    card1.cardText = "blablabla"
-    card1.cardType = "Introduction"
-    
-    //make new cards
-    
-    deck.addToDeckRelationship(card1)
-    
-    
-    do{
-        try self.context.save()
-    }catch{
-        
-    }
-}
-
-}
+//    deck1.deckName = "Product"
+////    let deck = Deck(context: context)
+////    deck2.deckName = "Product"
+//
+//    // Add time
+//    let card1 = Card(context: context)
+//    card1.cardCategory = "Introducing"
+//    card1.cardIndex = 1
+//    card1.cardTitle = "Product"
+//    card1.cardText = "blablabla"
+//    card1.cardType = "Introduction"
+//
+//    //make new cards
+//
+//    deck.addToDeckRelationship(card1)
+//
+//
+//    do{
+//        try self.context.save()
+//    }catch{
+//
+//    }
+//}
+//
+//}
